1d20e9cef4cad9da56da39a9796fa0f2318ba85d
pegomez5 <pegomez@ursinus.edu>
2025-10-18 12:45:45 -0400

    Added components folder, and example nav

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+'use client';
+import React from "react";
+
+// EXAMPLE NAV BAR 
+
+const navItems = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+  { name: "Services", href: "/services" },
+  { name: "Contact", href: "/contact" },
+];
+
+export default function Navbar() {
+  return (
+    <nav className="
+      sticky top-0 z-5 flex w-full items-center 
+      justify-start gap-x-8 p-4 bg-transparent
+      backdrop-blur-sm rounded-2xl">
+
+      {navItems.map((item) => (
+        <a
+          key={item.name}
+          href={item.href}
+          className="text-white hover:text-red-400 px-3 py-2 max-w-sm bg-slate-900 rounded-md text-sm font-medium transition-colors"
+        >
+          {item.name}
+        </a>
+      ))}
+
+    </nav>
+  );
+}
